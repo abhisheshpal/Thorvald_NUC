2dec0f949da989486d149ad7aa2ea0b05601b67e
Vignesh <Vignesh-Raja.Ponnambalam@eleves.ec-nantes.fr>
2018-02-25 20:29:03 +0100

    Add files via upload

--- src/thorvald_2d_nav/src/guinea_pig.cpp
+++ src/thorvald_2d_nav/src/guinea_pig.cpp
@@ -6,7 +6,6 @@
 #include <tf/tf.h>
 #include <algorithm>
 #include <cmath>
-#include <math.h>       /* fabs */
 #include <visualization_msgs/Marker.h>
 
 // ROS message includes
@@ -16,13 +15,13 @@
 // Parameters 
 geometry_msgs::Point n;
 int k; // iterations need to find the best model
-double thershold = 0.3;
-int d = 15; // nearby point to fit the line
+double thershold = 0.8;
+int d = 10; // nearby point to fit the line
 
 double best_model;
 double x_1[1080], y_1[1080], angle_1, x_2[1080], y_2[1080], angle_2;
 sensor_msgs::LaserScan scan_msg_main;
-int p_1 = 1, l_1 = 1, c_1 = 0, a_1 = 0, b_1 = 0, p_2 = 1, l_2 = 1, c_2 = 0, a_2 = 0, b_2 = 0;
+int p_1 = 0, l_1 = 0, c_1 = 0, a_1 = 0, b_1 = 0, p_2 = 0, l_2 = 0, c_2 = 0, a_2 = 0, b_2 = 0;
 double current_range_1, current_range_2;
 int final_count_1 = 0, final_count_2 = 0, final_count_3 = 0, final_count_4 = 0, final_count_5 = 0, final_count_6 = 0, finale = 0;
 int count_i_1[1080], count_i_2[1080];
@@ -65,12 +64,12 @@ void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
 Point Line_detection_1(sensor_msgs::LaserScan scan_msgs, Point* line_pt_1){
 
         // x, y, theta calculation
-	for (int i_1 = 0; i_1 <= 60; i_1++){
+	for (int i_1 = 300; i_1 <= 360; i_1++){
 	 angle_1 = scan_msg_main.angle_min +i_1*scan_msg_main.angle_increment; 
 	 x_1[i_1] = scan_msg_main.ranges[i_1]*cos(angle_1);
 	 y_1[i_1] = scan_msg_main.ranges[i_1]*sin(angle_1);
                 
-          if(!std::isnan(x_1[i_1]) && !std::isnan(y_1[i_1]) && (scan_msg_main.ranges[i_1] < 1.5)){
+          if(!std::isnan(x_1[i_1]) && !std::isnan(y_1[i_1]) && (scan_msg_main.ranges[i_1] < 1.0)){
            count_i_1[l_1] = i_1;
            l_1= l_1 + 1;
           } // storing the ith value with pre-conditions
@@ -100,8 +99,7 @@ Point Line_detection_1(sensor_msgs::LaserScan scan_msgs, Point* line_pt_1){
             int index_1 = count_i_1[i_1];
             double error_1 = y_1[index_1] - (model_1.m*x_1[index_1] + model_1.b); // error calculation
 
-            if((thershold > error_1) && (-thershold < error_1) && (error_1 != 0)){  // check for threshold
-            // std::cout << "error_1:" << abs(error_1) << "\n" << std::endl;
+            if(thershold > abs(error_1) && (error_1 != 0)){  // check for threshold
               current_best_inlierpoints_1[p_1].real_x = x_1[index_1];
               current_best_inlierpoints_1[p_1].real_y = y_1[index_1];
               p_1 = p_1 +1;
@@ -115,17 +113,29 @@ Point Line_detection_1(sensor_msgs::LaserScan scan_msgs, Point* line_pt_1){
 
         if(final_count_1 > d){ // selecting the inliers with max of points    
 
-     if(final_count_1 > final_count_2){
-            if(fabs(x_1[aIndex_1]-x_1[bIndex_1]) > thershold){
+         if(c_1 == 0){
+           final_count_2 = final_count_1;
+           c_1 = 1;
+           } 
+
+         if(c_1 == 1 && final_count_1 > final_count_2){
+           final_count_3 = final_count_1;
+           }
+
+         if(final_count_3 > final_count_2){
             final_Index_1[1].real_x = x_1[aIndex_1];
             final_Index_1[1].real_y = y_1[aIndex_1];
             final_Index_1[2].real_x = x_1[bIndex_1];
-            final_Index_1[2].real_y = y_1[bIndex_1];
-            } 
+            final_Index_1[2].real_y = y_1[bIndex_1]; 
            }
-
-             final_count_2 = final_count_1;
-
+         else{
+            final_Index_1[1].real_x = x_1[aIndex_1];
+            final_Index_1[1].real_y = y_1[aIndex_1];
+            final_Index_1[2].real_x = x_1[bIndex_1];
+            final_Index_1[2].real_y = y_1[bIndex_1]; 
+           }
+             std::cout << "final_count_2:" << final_count_2 << "\n" << std::endl;
+             std::cout << "final_count_3:" << final_count_3 << "\n" << std::endl;
                  
           } // max inliers selection end
 
@@ -138,20 +148,20 @@ Point Line_detection_1(sensor_msgs::LaserScan scan_msgs, Point* line_pt_1){
 Point Line_detection_2(sensor_msgs::LaserScan scan_msgs, Point* line_pt_2){
 
         // x, y, theta calculation
-	for (int i_2 = 300; i_2 <= 360; i_2++){
+	for (int i_2 = 0; i_2 <= 45; i_2++){
 	angle_2 = scan_msg_main.angle_min+i_2*scan_msg_main.angle_increment;
 	x_2[i_2] = scan_msg_main.ranges[i_2]*cos(angle_2);
 	y_2[i_2] = scan_msg_main.ranges[i_2]*sin(angle_2);
 
-          if(!std::isnan(x_2[i_2]) && !std::isnan(y_2[i_2]) && (scan_msg_main.ranges[i_2] < 1.6)){
+          if(!std::isnan(x_2[i_2]) && !std::isnan(y_2[i_2]) && (scan_msg_main.ranges[i_2] < 1.5)){
            count_i_2[l_2] = i_2;
            l_2 = l_2 + 1;                
           } // storing the ith value with pre-conditions
          } // storing the range values (end for loop)
-
+        // ROS_INFO("hereeeeeee");
         l_2= l_2 - 1;
-        int i_corr_x_2 = rand()% (l_2);
-        int i_corr_y_2 = rand()% (l_2);
+        int i_corr_x_2 = rand()% (l_2-1);
+        int i_corr_y_2 = rand()% (l_2-1);
         int aIndex_2 = count_i_2[i_corr_x_2]; 
         int bIndex_2 = count_i_2[i_corr_y_2];
 
@@ -168,11 +178,11 @@ Point Line_detection_2(sensor_msgs::LaserScan scan_msgs, Point* line_pt_2){
 
         if(count_2 == 0){   // check for aIndex and bIndex values
 
-        for (int i_2 = 0; i_2 < (l_2); i_2++){
+        for (int i_2 = 0; i_2 < (l_2-1); i_2++){
            int index_2 = count_i_2[i_2];
            double error_2 = y_2[index_2] - (model_2.m*x_2[index_2] + model_2.b); // error calculation
 
-           if((thershold > error_2) && (error_2 > (-thershold))  && (error_2!= 0)){
+           if(thershold > abs(error_2) && error_2 != 0){
            current_best_inlierpoints_2[p_2].real_x = x_2[index_2];
            current_best_inlierpoints_2[p_2].real_y = y_2[index_2];
            p_2 = p_2 +1;
@@ -184,18 +194,30 @@ Point Line_detection_2(sensor_msgs::LaserScan scan_msgs, Point* line_pt_2){
 
         if(final_count_4 > d){ // selecting the inliers with max of points    
 
-         if(final_count_4 > final_count_5){
-            if(fabs(x_2[aIndex_2]-x_2[bIndex_2]) > thershold){
+         if(c_2 == 0){
+           final_count_5 = final_count_4;
+           c_2 = 1;
+           } 
+
+         if(c_2 == 1 && final_count_4 > final_count_5){
+           final_count_6 = final_count_5;
+           }
+
+         if(final_count_6 > final_count_5){
             final_Index_2[1].real_x = x_2[aIndex_2];
             final_Index_2[1].real_y = y_2[aIndex_2];
             final_Index_2[2].real_x = x_2[bIndex_2];
-            final_Index_2[2].real_y = y_2[bIndex_2];
-                              // std::cout << "final_Index_2[2].real_x:" << final_Index_2[2].real_x << "\n" << std::endl;
-            } 
+            final_Index_2[2].real_y = y_2[bIndex_2]; 
            }
-            
-            final_count_5 = final_count_4;
-
+         else{
+            final_Index_2[1].real_x = x_2[aIndex_2];
+            final_Index_2[1].real_y = y_2[aIndex_2];
+            final_Index_2[2].real_x = x_2[bIndex_2];
+            final_Index_2[2].real_y = y_2[bIndex_2]; 
+           }
+             std::cout << "final_count_5:" << final_count_5 << "\n" << std::endl;
+             std::cout << "final_count_6:" << final_count_6 << "\n" << std::endl;
+                 
           } // max inliers selection end
 
         // Assigning variables to zero to start over
@@ -236,10 +258,11 @@ int main(int argc, char** argv)
         ros::ServiceServer service = n.advertiseService("sub_goal_check", add);
 
         while (ros::ok()){
+
 	ros::spinOnce();
 
         if(scan_msg_main.ranges.size() > 0 && (finale == 0)){
-         for (k=0; k <= 20000; k++){ // Number of iterations
+         for (k=0; k <= 10000; k++){ // Number of iterations
           Line_detection_1(scan_msg_main, final_Index_1);
           Line_detection_2(scan_msg_main, final_Index_2);
           }
@@ -248,7 +271,7 @@ int main(int argc, char** argv)
 
         if ( (final_count_1 != 0) && (line_strip_1.points.size() < 2) && (!boost::empty(final_Index_1))){ 
         line_strip_1.header.frame_id = "/laser";
-        // line_strip_1.header.stamp = ros::Time::now();
+        line_strip_1.header.stamp = ros::Time::now();
         line_strip_1.action = visualization_msgs::Marker::ADD;
         line_strip_1.pose.orientation.w = 1.0;
         line_strip_1.type = visualization_msgs::Marker::LINE_STRIP;
@@ -272,10 +295,12 @@ int main(int argc, char** argv)
            line_strip_1.points.push_back(pt_1); }            
          } // for loop end for storing points
 
+
         } // count check
 
-      if ( (final_count_4 != 0) && (line_strip_2.points.size() < 2) && (!boost::empty(final_Index_2))){ 
+        if ( (final_count_2 != 0) && (line_strip_2.points.size() < 2) && (!boost::empty(final_Index_2))){ 
         line_strip_2.header.frame_id = "/laser";
+        line_strip_2.header.stamp = ros::Time::now();
         line_strip_2.action = visualization_msgs::Marker::ADD;
         line_strip_2.pose.orientation.w = 1.0;
         line_strip_2.type = visualization_msgs::Marker::LINE_STRIP;
@@ -287,22 +312,23 @@ int main(int argc, char** argv)
         // Line strip is blue
         line_strip_2.color.b = 1.0;
         line_strip_2.color.a = 1.0;
-// ROS_INFO("afaafa");
+
         // Create the vertices for the points and lines
          for(int q_2 = 1; q_2 <= 2; q_2++){
         geometry_msgs::Point pt_2;
 
         pt_2.x = final_Index_2[q_2].real_x; 
         pt_2.y = final_Index_2[q_2].real_y; 
-
+  
           if(pt_2.x != 0 && pt_2.y != 0){
            line_strip_2.points.push_back(pt_2); }            
          } // for loop end for storing points
 
         } // count check
- 
-      if(line_strip_1.points.size() > 1 && line_strip_2.points.size() > 1 && (final_line.points.size() < 2)){
+
+      if(line_strip_1.points.size() > 1 && line_strip_2.points.size() > 1){
         final_line.header.frame_id = "/laser";
+        final_line.header.stamp = ros::Time::now();
         final_line.action = visualization_msgs::Marker::ADD;
         final_line.pose.orientation.w = 1.0;
         final_line.type = visualization_msgs::Marker::LINE_STRIP;
@@ -313,42 +339,27 @@ int main(int argc, char** argv)
         final_line.scale.x = 0.03;
 
         // Line strip is blue
-        final_line.color.r = 1.0;
+        final_line.color.b = 1.0;
         final_line.color.a = 1.0;
 
         // Create the vertices for the points and lines
-
-        geometry_msgs::Point pt_3[2];
-        if (fabs(final_Index_1[1].real_x - final_Index_2[1].real_x) < thershold){
-        pt_3[1].x = (final_Index_1[1].real_x + final_Index_2[1].real_x)/2; 
-        pt_3[1].y = (final_Index_1[1].real_y + final_Index_2[1].real_y)/2; 
-        pt_3[2].x = (final_Index_1[2].real_x + final_Index_2[2].real_x)/2; 
-        pt_3[2].y = (final_Index_1[2].real_y + final_Index_2[2].real_y)/2; 
-        }
-        else{
-        pt_3[1].x = (final_Index_1[1].real_x + final_Index_2[2].real_x)/2; 
-        pt_3[1].y = (final_Index_1[1].real_y + final_Index_2[2].real_y)/2; 
-        pt_3[2].x = (final_Index_1[2].real_x + final_Index_2[1].real_x)/2; 
-        pt_3[2].y = (final_Index_1[2].real_y + final_Index_2[1].real_y)/2; 
-        }
-
-        for (int q_3=1; q_3 <= 2; q_3++){
-        if(pt_3[1].x != 0 && pt_3[1].y != 0){
-         final_line.points.push_back(pt_3[q_3]); }  
-        }
-
+         for(int q_3 = 1; q_3 <= 2; q_3++){
+        geometry_msgs::Point pt_3;
+        pt_3.x = (final_Index_1[q_3].real_x + final_Index_2[q_3].real_x)/2; 
+        pt_3.y = (final_Index_1[q_3].real_y + final_Index_2[q_3].real_y)/2; 
+  
+        if(pt_3.x != 0 && pt_3.y != 0){
+         final_line.points.push_back(pt_3); }  
+         } // for loop end for storing points          
 
        } // count check
-      
-        line_strip_1.header.stamp = ros::Time::now();
-        line_strip_2.header.stamp = ros::Time::now();
-        final_line.header.stamp = ros::Time::now();
+
 
         marker_pub_1.publish(line_strip_1);
         marker_pub_2.publish(line_strip_2);
         marker_pub_3.publish(final_line);
-        r.sleep();
 	} // node shutdown
+  r.sleep();
   return 0;
 }
 
